baceb802b48c76985763f2855a5e3ed00cd3425c
Alif102 <alifahmed102@gmail.com>
2023-10-19 17:50:56 +0600

    Register work done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,9 @@
-import { NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
+import UseAuth from "../Hooks/UseAuth";
 
 
 const Navbar = () => {
+  const {user , LogOut} = UseAuth();
     const navLinks =  <>
     <li><NavLink to="/">Home</NavLink></li>
     <li><NavLink to="/update">Update Product</NavLink></li>
@@ -35,7 +37,31 @@ const Navbar = () => {
      </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Login</a>
+  {
+                        user?.email ? <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img src={user.photoURL} alt={user.displayName} />
+                                </div>
+                            </label>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                <li>
+                                    <button className="btn btn-sm  btn-ghost">{user.displayName}</button>
+
+                                </li>
+                                <li>
+                                    <button className="btn btn-sm  btn-ghost"
+                                        onClick={LogOut}
+                                    >Logout</button>
+
+                                </li>
+                                </ul>
+                        </div>
+                            :
+                            <Link to='/login'>
+                                <button className="btn btn-sm  btn-ghost">Login</button>
+                            </Link>
+                    }
   </div>
 </div>
     </div>
